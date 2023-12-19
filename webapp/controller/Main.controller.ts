a2827060d35e4b4a6bc91206222907fb89d6b273
Victor Duong <victor.duong@simplemdg.com>
2023-12-19 17:40:48 +0700

    first commit

--- webapp/controller/Main.controller.ts
+++ webapp/controller/Main.controller.ts
@@ -1,11 +1,49 @@
 import MessageBox from "sap/m/MessageBox";
 import BaseController from "./BaseController";
+import JSONModel from "sap/ui/model/json/JSONModel";
+import Event from "sap/ui/base/Event"
+import { List$SelectionChangeEvent } from "sap/ui/webc/main/List";
+import Input from "sap/m/Input";
+import { ListBase$SelectionChangeEvent } from "sap/m/ListBase";
+import CustomListItem from "sap/m/CustomListItem";
 
 /**
  * @namespace sap.datalist.app.controller
  */
 export default class Main extends BaseController {
-	public sayHello(): void {
-		MessageBox.show("Hello World!");
+	private oModel = new JSONModel({
+        data:[]
+    });
+    onInit(): void {
+ 
+        // Set the model to the view
+        this.getView().setModel(this.oModel, "data");
+        this.getAllData();
+    }
+
+	onSelectionChange (event: ListBase$SelectionChangeEvent){
+		
+		let selectItem = event.getParameter("listItem") as CustomListItem;
+		console.log(selectItem.getContent())
+		selectItem.getContent()[0].setProperty("editable", true);
 	}
+
+	pressEdit (){
+		
+	}
+
+    async getAllData(){
+        try {
+            let response = await fetch(`https://api.restful-api.dev/objects`);
+            const oData = await response.json();
+            this.oModel.setData({
+                data: [...oData]
+            });
+        } catch (error) {
+            console.log(error)
+        }
+        finally{
+            console.log("finally");
+        }
+    };
 }
